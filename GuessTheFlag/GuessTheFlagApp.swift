90246869a30fa2c8fac9e5cf3a5702b342bcddfd
John Kostanov <kostanovd@icloud.com>
2021-07-15 11:56:13 +0200

    Initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Джон Костанов on 15/7/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
