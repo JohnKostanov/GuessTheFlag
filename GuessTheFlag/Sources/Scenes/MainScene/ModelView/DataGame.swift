be58a15862e7982645ca71624be6556b93f06f18
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2023-08-19 09:26:22 +0200

    Structured project

---
+++ GuessTheFlag/Sources/Scenes/MainScene/ModelView/DataGame.swift
@@ -0,0 +1,12 @@
+//
+//  DataGame.swift
+//  GuessTheFlag
+//
+//  Created by Джон Костанов on 19/8/23.
+//
+
+import Foundation
+
+struct DataGame {
+    
+}
--- GuessTheFlag/Sources/Scenes/MainScene/View/ContentView.swift
+++ GuessTheFlag/Sources/Scenes/MainScene/View/ContentView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
-    
     @State private var score = 0
     
     @State var animationAmount = 0.0
